38a942ca230d266c6570c3773b61428835cb60d0
WinnieCWu <winniechn7@gmail.com>
2022-03-17 18:10:55 -0700

    set up routes from homepage to app

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const router = require("express").Router();
+const path = require("path");
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  //write fxn to db.json
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    //convert the JS data array as JSON
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  //return finished code to post route for response
+  return note;
+}
+
+//1 job: respond with an HTML pg to display in the browser
+router.get("/notes", (req, res) => {
+  //tells where to find the file we want our server to read and send back to clt
+  //api routes will only interact with data //html routes will link to diff html files
+  //convert the JS data array as JSON
+  const notes = fs.readFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8"
+  );
+  //return JSON structure, rather than text
+  res.json(JSON.parse(notes));
+});
+
+router.get("*", (req, res) => {});
+
+router.post("/notes", (req, res) => {
+  //**save to req.body with unique id, add it to db.json, and return new note to clt.
+  req.body.id = notes.length.toString();
+  createNewNote();
+  //add notes to db.json file and notes array in this fxn
+  if (!validateNote(req.body)) {
+    res.status(400).set("The note is not properly formatted.");
+  } else {
+    const note = createNewNotes(req.body, notes);
+    res.json(note);
+  }
+});
+
+//
+router.delete("/notes:id", (req, res) => {
+  //or '/api/notes/{id}'
+  // const idNote = notes.
+  //**read all notes from db.json file, remove note with given id prop, and rewrite notes to db.json file
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-const path = require('path');
 const express = require('express');
 
 //will read the index.js files of each indicated directory
-const apiRoutes = require('./routes/apiRoutes/notes.js');
+const apiRoutes = require('./routes/apiRoutes/index.js');
 const htmlRoutes = require('./routes/htmlRoutes/index.js');
 
 const PORT = process.env.PORT || 3001;
@@ -22,34 +21,6 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
-//1 job: respond with an HTML pg to display in the browser
-app.get('/api/notes', (req, res) => {
-    //tells where to find the file we want our server to read and send back to clt
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('*', (req, res) => {
-    res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    //**save to req.body with unique id, add it to db.json, and return new note to clt.
-    req.body.id = notes.length.toString();
-    //add notes to db.json file and notes array in this fxn
-    if(!validateNote(req.body)) {
-        res.status(400).set('The note is not properly formatted.')
-    } else {
-        const note = createNewNotes(req.body, notes)
-        res.json(note);
-    }
-});
-
-//
-app.delete('/api/notes:id', (req, res) => {
-    //or '/api/notes/{id}'
-    // const idNote = notes.
-    //**read all notes from db.json file, remove note with given id prop, and rewrite notes to db.json file
-})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
