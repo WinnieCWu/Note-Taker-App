19c8f260ddb10f734da44513bf6f980cd6f57ad3
WinnieCWu <winniechn7@gmail.com>
2022-03-22 17:47:25 -0700

    all the routes are functioning

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 const router = require("express").Router();
 const path = require("path");
-const uuid = require('uuid');
-let notes = require("../../db/db.json");
+const { v4:uuid } = require('uuid');
+// let notes = require("../../db/db.json");
 
-function createNewNote(body, noteArray) {
-  const note = body;
-  noteArray.push(note);
-  //write fxn to db.json
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    //convert the JS data array as JSON
-    JSON.stringify({ note: noteArray }, null, 2)
-  );
-  //return finished code to post route for response
-  return note;
-}
+// function createNewNote(body, noteArray) {
+//   const note = body;
+//   noteArray.push(note);
+//   //write fxn to db.json
+//   fs.writeFileSync(
+//     path.join(__dirname, "./db/db.json"),
+//     //convert the JS data array as JSON
+//     JSON.stringify({ note: noteArray }, null, 2)
+//   );
+//   //return finished code to post route for response
+//   return note;
+// }
 
 //1 job: respond with an HTML pg to display in the browser
 router.get("/notes", (req, res) => {
@@ -40,29 +40,32 @@ router.post("/notes", (req, res) => {
   //since we don't get id upon posting
   //*how to properly use UUID or shortUUID? */ uuid.v4()?
   const pushNote = req.body;
-  notes.push(pushNote);
-  fs.writeFile("./db/db.json", JSON.stringify(notes), (err, data) => {
+  pushNote.id = uuid();
+  const note = JSON.parse(fs.readFileSync(
+      path.join(__dirname, "../../db/db.json"),
+      "utf-8"
+    ));
+  note.push(pushNote);
+  fs.writeFile("./db/db.json", JSON.stringify(note), (err, data) => {
     if (err) throw err;
   });
-  res.json(notes);
+  res.json(note);
 });
 //console.log( `Test for v4 (random) uuid: ${uuid.v4()}`);
 
 router.delete("/notes/:id", (req, res) => {
   const deleteNote = req.params.id;
-  notes.pop(deleteNote);
-  readFromFile('./db/db.json')
-  //**grab the noteId from post route to delete
-  .then((data) => JSON.parse(data))
-  .then((JSON) => {
-      const newNoteArray = JSON.filter(note => note.id !== noteId)
-      fs.writeToFile('./db/db.json', newNoteArray)
+  //want all notes where id # is not equal to the id param
+  const noteArray = JSON.parse(fs.readFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8"
+  ));
+  const filteredNotes = noteArray.filter(note=> note.id != deleteNote)
+      fs.writeFile('./db/db.json', JSON.stringify(filteredNotes), (err, data) => {
+        if (err) throw err;
+      });
       res.status(200).json('Note Deleted');
       return;
-  // fs.unlink("./db/db.json", JSON.parse(notes).filter(), (err, data) => {
-  //   if (err) throw err;
-  //   console.log("Successfully deleted note!");
-  });
 });
 
 module.exports = router;
